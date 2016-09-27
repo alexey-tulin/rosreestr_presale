3433b1bf4c89bdb6d5748c51e5d78da9c00bc9de
KatrinaBosh <katrinabosh@gmail.ru>
2016-09-28 00:13:45 +0400

    add handlers

---
+++ src/main/java/ru/rosreestr/client/isur/handler/IsurLoggerHandler.java
@@ -0,0 +1,33 @@
+package ru.rosreestr.client.isur.handler;
+
+import javax.xml.namespace.QName;
+import javax.xml.ws.handler.MessageContext;
+import javax.xml.ws.handler.soap.SOAPHandler;
+import javax.xml.ws.handler.soap.SOAPMessageContext;
+import java.util.Set;
+
+/**
+ * Created by KatrinaBosh on 28.09.2016.
+ */
+public class IsurLoggerHandler implements SOAPHandler<SOAPMessageContext> {
+
+    public Set<QName> getHeaders() {
+        // todo
+        return null;
+    }
+
+    public boolean handleMessage(SOAPMessageContext context) {
+        // todo
+        return false;
+    }
+
+    public boolean handleFault(SOAPMessageContext context) {
+        // todo
+        return false;
+    }
+
+    public void close(MessageContext context) {
+        // todo
+
+    }
+}
