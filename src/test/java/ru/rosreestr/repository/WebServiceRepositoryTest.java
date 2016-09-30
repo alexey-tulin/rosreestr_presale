bb738a32100b2044ce0703f337838a5ceb871848
KatrinaBosh <katrinabosh@gmail.ru>
2016-09-30 10:23:51 +0400

    handler on, add test

---
+++ src/test/java/ru/rosreestr/repository/WebServiceRepositoryTest.java
@@ -0,0 +1,24 @@
+package ru.rosreestr.repository;
+
+import junit.framework.TestCase;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import ru.rosreestr.config.AppConfig;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {AppConfig.class})
+public class WebServiceRepositoryTest extends TestCase {
+
+    public void testLog() throws Exception {
+
+    }
+
+    public void testLog1() throws Exception {
+
+    }
+
+    public void testLogXml() throws Exception {
+
+    }
+}
