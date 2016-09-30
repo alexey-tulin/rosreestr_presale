84c6d422d09433905b5c629bcda465954ff194e4
Tatiana Chukina <tatiana.zhabenko@gmail.com>
2016-09-30 13:58:19 +0400

    Added data to sendTask service

--- src/main/java/ru/rosreestr/client/isur/processor/ServiceImpl.java
+++ src/main/java/ru/rosreestr/client/isur/processor/ServiceImpl.java
@@ -5,11 +5,9 @@ import com.sun.xml.internal.ws.api.message.Headers;
 import com.sun.xml.internal.ws.developer.WSBindingProvider;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import ru.rosreestr.client.isur.IService;
 import ru.rosreestr.client.isur.Service;
-import ru.rosreestr.client.isur.model.CoordinateTaskData;
-import ru.rosreestr.client.isur.model.ObjectFactory;
+import ru.rosreestr.client.isur.model.*;
 import ru.rosreestr.utils.CommonUtils;
 
 import javax.xml.bind.JAXBElement;
@@ -37,10 +35,88 @@ public class ServiceImpl {
     public void sendTask() {
         LOG.info("start service sendTask");
         IService customBindingIService = service.getCustomBindingIService();
-        CoordinateTaskData coordinateTaskData = createCoordinateTaskData();
-        addHeader((WSBindingProvider) customBindingIService, createSendTaskHeaders());
+        String serviceNumber = String.format(SERVICE_NUMBER_TEMPLATE, getNewMessageNum(), CommonUtils.getCurrentYear());
+        CoordinateTaskData coordinateTaskData = createCoordinateTaskData(serviceNumber);
+        addHeader((WSBindingProvider) customBindingIService, createSendTaskHeaders(serviceNumber));
         customBindingIService.sendTask(coordinateTaskData);
         LOG.info("end service sendTask");
+    }
+
+    private CoordinateTaskData createCoordinateTaskData(String serviceNumber) {
+        CoordinateTaskData coordinateTaskData = new CoordinateTaskData();
+        RequestTask requestTask = createRequestTask(serviceNumber);
+        DocumentsRequestData documentsRequestData = createDocumentsRequestData();
+        Object signature = createSignature();
+
+        coordinateTaskData.setTask(requestTask);
+        coordinateTaskData.setData(documentsRequestData);
+        coordinateTaskData.setSignature(signature);
+        return coordinateTaskData;
+    }
+
+
+    private RequestTask createRequestTask(String serviceNumber) {
+        RequestTask requestTask = new RequestTask();
+        requestTask.setRequestId(UUID.randomUUID().toString());
+        requestTask.setServiceNumber(serviceNumber);
+        requestTask.setServiceTypeCode("047202");
+
+        Person responsible = new Person();
+        responsible.setLastName("Столыпин");
+        responsible.setFirstName("Петр");
+        responsible.setMiddleName("Аркадьевич");
+        responsible.setJobTitle("АСУ ЕИРЦ");
+        responsible.setPhone("8-999-999-99-99");
+        responsible.setEmail("test@test.test");
+        requestTask.setResponsible(responsible);
+
+        Department department = new Department();
+        department.setName("Департамент информационных технологий города Москвы");
+        department.setCode("707");
+        department.setInn("7710878000");
+        department.setOgrn("1107746943347");
+        requestTask.setDepartment(department);
+
+        return requestTask;
+    }
+
+    //todo PARAMETER
+    private DocumentsRequestData createDocumentsRequestData() {
+        DocumentsRequestData documentsRequestData = new DocumentsRequestData();
+        documentsRequestData.setDocumentTypeCode("77290");
+        documentsRequestData.setIncludeXmlView(true);
+        documentsRequestData.setIncludeBinaryView(true);
+        documentsRequestData.setParameterTypeCode("77290");
+        DocumentsRequestData.Parameter parameter = createParameter();
+        documentsRequestData.setParameter(parameter);
+        return documentsRequestData;
+    }
+
+    //TODO fill in
+    private DocumentsRequestData.Parameter createParameter() {
+        DocumentsRequestData.Parameter parameter = new DocumentsRequestData.Parameter();
+
+        return parameter;
+    }
+
+    //TODO fill in
+    private Object createSignature() {
+        return null;
+    }
+
+    private String getNewMessageNum() {
+        //TODO retrieve from DB
+        return "000000";
+    }
+
+    private ru.rosreestr.client.isur.model.Headers createSendTaskHeaders(String serviceNumber) {
+        ru.rosreestr.client.isur.model.Headers headers = new ru.rosreestr.client.isur.model.Headers();
+        headers.setFromOrgCode(FROM_ORG_CODE);
+        headers.setToOrgCode(TO_ORG_CODE);
+        headers.setRequestDateTime(CommonUtils.getXmlGregorianCurrentDate());
+        headers.setMessageId(UUID.randomUUID().toString());
+        headers.setServiceNumber(serviceNumber);
+        return headers;
     }
 
     private void addHeader(WSBindingProvider customBindingIService, ru.rosreestr.client.isur.model.Headers headers) {
@@ -54,25 +130,4 @@ public class ServiceImpl {
             LOG.error(e.getMessage(), e);
         }
     }
-
-    private CoordinateTaskData createCoordinateTaskData() {
-        CoordinateTaskData coordinateTaskData = new CoordinateTaskData();
-        //TODO fill in
-        return coordinateTaskData;
-    }
-
-    private ru.rosreestr.client.isur.model.Headers createSendTaskHeaders() {
-        ru.rosreestr.client.isur.model.Headers headers = new ru.rosreestr.client.isur.model.Headers();
-        headers.setFromOrgCode(FROM_ORG_CODE);
-        headers.setToOrgCode(TO_ORG_CODE);
-        headers.setRequestDateTime(CommonUtils.getXmlGregorianCurrentDate());
-        headers.setMessageId(UUID.randomUUID().toString());
-        headers.setServiceNumber(String.format(SERVICE_NUMBER_TEMPLATE, "000000", CommonUtils.getCurrentYear()));//TODO -000000 NNNNNN - порядковый номер обращения за указанной государственной услугой в организации, зарегистрировавшей обращение, в текущем году;
-        return headers;
-    }
-
-    public Service getService() {
-        return service;
-    }
-
 }
