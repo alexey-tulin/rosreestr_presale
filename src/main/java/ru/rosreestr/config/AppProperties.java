4fc6a0513108dfac06e5767effdca0bb9440af55
KatrinaBosh <katrinabosh@gmail.ru>
2016-10-01 21:10:05 +0400

    add properties

--- src/main/java/ru/rosreestr/config/AppProperties.java
+++ src/main/java/ru/rosreestr/config/AppProperties.java
@@ -3,14 +3,21 @@ package ru.rosreestr.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-/**
- * Created by KatrinaBosh on 28.09.2016.
- */
+
 @Component
 public class AppProperties {
 
     @Value("${ws.client.isur}")
     private String isur;
+
+    @Value("${signature.provider}")
+    private String signatureProvider;
+
+    @Value("${signature.alias}")
+    private  String signatureAlias;
+
+    @Value(("${signature.password}"))
+    private String signaturePassword;
 
     public String getIsur() {
         return isur;
@@ -18,5 +25,29 @@ public class AppProperties {
 
     public void setIsur(String isur) {
         this.isur = isur;
+    }
+
+    public String getSignatureAlias() {
+        return signatureAlias;
+    }
+
+    public void setSignatureAlias(String signatureAlias) {
+        this.signatureAlias = signatureAlias;
+    }
+
+    public String getSignaturePassword() {
+        return signaturePassword;
+    }
+
+    public void setSignaturePassword(String signaturePassword) {
+        this.signaturePassword = signaturePassword;
+    }
+
+    public String getSignatureProvider() {
+        return signatureProvider;
+    }
+
+    public void setSignatureProvider(String signatureProvider) {
+        this.signatureProvider = signatureProvider;
     }
 }
